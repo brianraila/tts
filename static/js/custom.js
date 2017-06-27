21a770eeb91bd307f29ab8a632fb59ff09b7c44a
African Dev <railamolo69@gmail.com>
2017-06-27 23:10:18 +0300

    Clean Up: Eliminated Unnecessary static assets

--- static/js/custom.js
+++
@@ -1,7 +0,0 @@
-function performSearch() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "Your Rest URL Here", false);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-}
